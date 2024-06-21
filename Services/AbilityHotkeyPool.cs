b68f7054585d5e1f5fb59c6a95fc6923dccc612b
Zakary Boven <zakaryboven@gmail.com>
2024-06-21 23:17:06 +1200

    Cleanup

---
+++ Services/AbilityHotkeyPool.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using WoWHotkeySolver.Enums;
+
+namespace WoWHotkeySolver.Services;
+
+/// <summary>
+/// A pool of abilities and hotkeys that can be allocated.
+/// </summary>
+public sealed class AbilityHotkeyPool
+{
+  public required List<Ability> Abilities { get; init; }
+  
+  public required List<Hotkey> Hotkeys { get; init; }
+
+  public void Remove(Ability ability, Hotkey hotkey)
+  {
+    Abilities.Remove(ability);
+    Hotkeys.Remove(hotkey);
+  }
+  
+  /// <summary>
+  /// Outputs a hotkey if it has been reserved for an ability of this type.
+  /// </summary>
+  public bool TryGetReservedHotkey(Ability ability, [NotNullWhen(true)] out Hotkey? hotkey)
+  {
+    hotkey = Hotkeys.FirstOrDefault(x =>
+      x.ReservedForAbilityType != AbilityType.Other && x.ReservedForAbilityType == ability.AbilityType);
+    return hotkey != null;
+  }
+}
--- Services/SolverService.cs
+++ Services/SolverService.cs
@@ -1,7 +1,4 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using WoWHotkeySolver.Enums;
-
-namespace WoWHotkeySolver.Services;
+﻿namespace WoWHotkeySolver.Services;
 
 public sealed class SolverService
 {
@@ -15,36 +12,38 @@ public sealed class SolverService
       return $"There aren't enough hotkeys ({Hotkeys.Count}) to cast that many abilities ({Abilities.Count}).";
 
     var solution = new HotkeySolution();
-    var orderedAbilities = Abilities.OrderBy(x => x.Frequency).ToList();
-    var orderedHotkeys = Hotkeys.OrderBy(x => x.Convenience).ToList();
+    var abilityHotkeyPool = new AbilityHotkeyPool
+    {
+      Abilities = Abilities.OrderBy(x => x.Frequency).ToList(),
+      Hotkeys = Hotkeys.OrderBy(x => x.Convenience).ToList()
+    };
 
-    foreach (var ability in Abilities)
-    {
-      if (TryGetReservedHotkey(ability, out var hotkey))
-      {
-        orderedAbilities.Remove(ability);
-        orderedHotkeys.Remove(hotkey);
-        solution.AddHotkey(ability, hotkey);
-      }
-    }
-    
-    foreach (var ability in orderedAbilities)
-    {
-      var hotkey = orderedHotkeys.First();
-      solution.AddHotkey(ability, hotkey);
-      orderedHotkeys.RemoveAt(0);
-    }
+    AllocateReservedHotkeys(abilityHotkeyPool, solution);
+    AllocateUnreservedHotkeys(abilityHotkeyPool, solution);
     
     return solution.ToString();
   }
 
-  /// <summary>
-  /// Outputs a hotkey if it has been reserved for an ability of this type.
-  /// </summary>
-  private bool TryGetReservedHotkey(Ability ability, [NotNullWhen(true)] out Hotkey? hotkey)
+  private static void AllocateReservedHotkeys(AbilityHotkeyPool abilityHotkeyPool, HotkeySolution solution)
   {
-    hotkey = Hotkeys.FirstOrDefault(x =>
-      x.ReservedForAbilityType != AbilityType.Other && x.ReservedForAbilityType == ability.AbilityType);
-    return hotkey != null;
+    foreach (var ability in abilityHotkeyPool.Abilities.ToList())
+    {
+      if (abilityHotkeyPool.TryGetReservedHotkey(ability, out var hotkey))
+      {
+        abilityHotkeyPool.Remove(ability, hotkey);
+        solution.AddHotkey(ability, hotkey);
+      }
+    }
+  }
+  
+  private static void AllocateUnreservedHotkeys(AbilityHotkeyPool abilityHotkeyPool, HotkeySolution solution)
+  {
+    var hotkeys = abilityHotkeyPool.Hotkeys.ToList();
+    foreach (var ability in abilityHotkeyPool.Abilities.ToList())
+    {
+      var hotkey = hotkeys.First();
+      solution.AddHotkey(ability, hotkey);
+      abilityHotkeyPool.Remove(ability, hotkey);
+    }
   }
 }
