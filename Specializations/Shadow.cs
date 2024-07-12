fb92055c58c8e68b159ff6532d3b334248a5b5cc
Zakary Boven <zakaryboven@gmail.com>
2024-07-12 23:13:44 +1200

    Add more Shadow spells

--- Specializations/Shadow.cs
+++ Specializations/Shadow.cs
@@ -13,7 +13,43 @@ public static class Shadow
       {
         Name = "Mindgames",
         Frequency = Frequency.Sometimes
-      }
+      },
+      new()
+      {
+        Name = "Devouring Plague",
+        Frequency = Frequency.Frequent
+      },
+      new()
+      {
+        Name = "Dispersion",
+        Frequency = Frequency.Sometimes,
+        AbilityType = AbilityType.PersonalDefensive
+      },
+      new()
+      {
+        Name = "Mind Flay",
+        Frequency = Frequency.Constant
+      },
+      new()
+      {
+        Name = "Mind Blast",
+        Frequency = Frequency.Constant
+      },
+      new()
+      {
+        Name = "Vampiric Touch",
+        Frequency = Frequency.Frequent
+      },
+      new()
+      {
+        Name = "Void Eruption/Void Bolt",
+        Frequency = Frequency.Sometimes
+      },
+      new()
+      {
+        Name = "Void Torrent",
+        Frequency = Frequency.Sometimes
+      },
     };
     abilities.AddRange(Priest.GetAbilities());
     return abilities;
